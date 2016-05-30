860445295593cdbacf022e6f04f624ae9afec7bb
Alva Chien <alvachien@live.com>
2016-05-30 18:04:09 +0800

    Initial commit

---
+++ src/acidserver/Configuration/ACProfileProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4;
+using IdentityServer4.Core;
+using IdentityServer4.Core.Services;
+using IdentityServer4.Core.Models;
+using IdentityServer4.Core.Configuration;
+using IdentityServer4.Core.Endpoints;
+using IdentityServer4.Core.Events;
+using IdentityServer4.Core.Extensions;
+using IdentityServer4.Core.Hosting;
+using IdentityServer4.Core.Resources;
+using IdentityServer4.Core.ResponseHandling;
+
+namespace acidserver.Configuration
+{
+    public class ACProfileProvider : IProfileService
+    {
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/acidserver/Startup.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using acidserver.Data;
+using acidserver.Models;
+using acidserver.Services;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Http;
+using acidserver.Extensions;
+
+namespace acidserver
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _environment = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cert = new X509Certificate2(Path.Combine(_environment.ContentRootPath, "idsrv3test.pfx"), "idsrv3test");
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.SigningCertificate = cert;
+            });
+
+            builder.AddCustomGrantValidator<CustomGrantValidator>();
+
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            //services.AddMvc();
+            services
+                .AddMvc()
+                .AddRazorOptions(razor =>
+                {
+                    //razor.ViewLocationExpanders.Add(new UI.CustomViewLocationExpander());
+                });
+            //services.AddTransient<UI.Login.LoginService>();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Temp",
+                AutomaticAuthenticate = false,
+                AutomaticChallenge = false
+            });
+
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                AuthenticationScheme = "Google",
+                SignInScheme = "Temp",
+                ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com",
+                ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo"
+            });
+
+            app.UseIdentityServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
