864297dac4ebd74ddd2f57908010b6fcc68aeb5e
Alva Chien <alvachien@163.com>
2020-03-25 15:47:27 +0800

    remove elder folder

--- src/acidserver/Elder/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="js/signout-redirect.js" />
-/// <reference path="js/site.js" />
-/// <reference path="lib/jquery/dist/jquery.slim.js" />
-/// <reference path="lib/popper.js/dist/umd/popper.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
